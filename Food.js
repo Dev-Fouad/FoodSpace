c9f2b321a5d0dafca9b3dcce8ad81d86176dda30
fouad ajayi <fouadajayi6@gmail.com>
2023-01-25 18:18:55 +0100

    Done and donerous😁💥

--- Food.js
+++ Food.js
@@ -1,10 +1,17 @@
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+let overlay = document.querySelector(".overlay")
 
 menu.addEventListener("click", function (){
     navbar.classList.toggle("active");
+    overlay.classList.toggle("hidden");
 });
 
 window.oncsroll = () => {
     navbar.classList.remove("active");
 };
+
+overlay.addEventListener('click', function(){
+    overlay.classList.add('hidden');
+    navbar.classList.remove("active");
+})
